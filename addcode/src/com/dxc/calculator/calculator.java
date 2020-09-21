caa468a3d7e025db97b1caed3718ef79ef02a14c
vbharath <vbharath@IN-5CG0251NTS>
2020-09-21 15:09:45 +0530

    first commit uploading all files

---
+++ addcode/src/com/dxc/calculator/calculator.java
@@ -0,0 +1,20 @@
+package com.dxc.calculator;
+
+public class calculator {
+	
+	public int add(int num1,int num2)
+	{
+		
+		int r = num1+num2;
+		return r;
+		
+	}
+	public int sub(int num1,int num2)
+	{
+		
+		int r = num1-num2;
+		return r;
+		
+	}
+
+}
